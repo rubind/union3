077e531b76925604a8dbfb8fde35e8dfa413bf7b
David Rubin <drubin@node-0005.hpc.ci.its.hawaii.edu>
2022-06-14 18:38:06 +0000

    rename

--- simulated/hpc_make_sim.sh
+++ simulated/hpc_make_sim.sh
@@ -8,5 +8,5 @@
 #SBATCH --error=example-%A.err # %A - filled with jobid, where to write the stderr
 #SBATCH --output=example-%A.out # %A - filled with jobid, wher to write the stdout
 source ~/.bash_profile
-cd /home/drubin/nfs_fs02/simulated_data_runs_no_int_no_twobeta_no_kcorrect
-python $UNITY/simulated/make_sim_multidataset.py 1 1 1 0 0 0 > log_sim.txt
+cd /home/drubin/nfs_fs02/simulated_data_runs_no_twobeta/
+python $UNITY/simulated/make_sim_multidataset.py 1 1 1 0 0 1 > log_sim.txt
