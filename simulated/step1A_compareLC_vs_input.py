0d9c301b0e9fca5dd183a1017b37c6a3e091202c
David Rubin <drubin@node-0093.hpc.ci.its.hawaii.edu>
2023-04-13 17:34:43 +0000

    plotting

--- simulated/step1A_compareLC_vs_input.py
+++ simulated/step1A_compareLC_vs_input.py
@@ -30,19 +30,19 @@ all_dat = dict(true_c = [], delta_c = [], obs_sig_c = [],
 for resfl in glob.glob("dataset_000/*/res*salt2.dat"):
     obs_c = read_param(resfl, "Color")
     if obs_c != None:
-        obs_sigc = read_param(resfl, "Color", ind = 2)
+        obs_sig_c = read_param(resfl, "Color", ind = 2)
 
 
         obs_x0 = read_param(resfl, "X0")
         obs_mag = -2.5*np.log10(obs_x0)
         obs_x1 = read_param(resfl, "X1")
-        obs_sigx1 = read_param(resfl, "X1", ind = 2)
+        obs_sig_x1 = read_param(resfl, "X1", ind = 2)
 
         
         redshift = read_param(resfl, "Redshift")
 
-        paramsfl = resfl.replace("/SN", "/SN_params/params_").replace("/result_salt2.dat", ".dat")
-        
+        paramsfl = resfl.replace("/SN", "/SN_params/params_").replace("/result_salt2.dat", ".dat").replace("dataset_", "UNITY_")
+
         
         true_x0 = read_param(paramsfl, "x0")
         true_mag = -2.5*np.log10(true_x0)
@@ -53,14 +53,14 @@ for resfl in glob.glob("dataset_000/*/res*salt2.dat"):
         
         all_dat["true_c"].append(true_c)
         all_dat["delta_c"].append(obs_c - true_c)
-        all_dat["obs_sigc"].append(obs_sigc)
+        all_dat["obs_sig_c"].append(obs_sig_c)
         
 
         all_dat["delta_mag"].append(obs_mag - true_mag)
 
         all_dat["true_x1"].append(true_x1)
         all_dat["delta_x1"].append(obs_x1 - true_x1)
-        all_dat["obs_sigx1"].append(obs_sigx1)
+        all_dat["obs_sig_x1"].append(obs_sig_x1)
 
 
         all_dat["delta_mu"].append((obs_mag + 0.14*obs_x1 - 3.1*obs_c) - (true_mag + 0.14*true_x1 - 3.1*true_c))
@@ -73,8 +73,8 @@ for resfl in glob.glob("dataset_000/*/res*salt2.dat"):
 for key in all_dat:
     all_dat[key] = np.array(all_dat[key])
 
-all_dat["pulls_c"] = all_dat["delta_c"]/all_dat["obs_sigc"]
-all_dat["pulls_x1"] = all_dat["delta_x1"]/all_dat["obs_sigx1"]
+all_dat["pulls_c"] = all_dat["delta_c"]/all_dat["obs_sig_c"]
+all_dat["pulls_x1"] = all_dat["delta_x1"]/all_dat["obs_sig_x1"]
 
 
 plt.figure(figsize = (24, 16))
