b062cde6a7fd9345a418b19cfeca679a9e338207
David Rubin <drubin@hawaii.edu>
2023-04-27 20:15:42 -1000

    read and sample plot

--- scripts/read_and_sample.py
+++ scripts/read_and_sample.py
@@ -415,9 +415,14 @@ def get_redshifts(redshifts):
 
 def plot_coeffs(z_list, redshift_coeffs):
     plt.figure(2)
-    for i in range(len(redshift_coeffs)):
-        for j in range(len(redshift_coeffs[0])):
-            plt.plot(z_list[i], redshift_coeffs[i][j], '.', color = "C" + str(j))
+
+    sqrtn = int(np.ceil(np.sqrt(len(redshift_coeffs[0]))))
+
+    plt.figure(figsize = (4*sqrtn, 3*sqrtn))
+    
+    for j in range(len(redshift_coeffs[0])):
+        plt.subplot(sqrtn, sqrtn, j+1)
+        plt.plot(z_list, redshift_coeffs[:,j], '.')
 
     plt.savefig("redshift_coeffs.pdf")
     plt.close()
