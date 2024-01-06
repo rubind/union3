23ca4904dfe542ec7fb7b9f5fc8736bbf093c030
David Rubin <drubin@hawaii.edu>
2024-01-06 07:11:46 -1000

    sampling check

--- simulated/step1B_Om_sampled_well.py
+++ simulated/step1B_Om_sampled_well.py
@@ -1,2 +1,31 @@
 try:
     from commands import getoutput
+except:
+    from subprocess import getoutput
+import sys
+import glob
+
+def check_param(logfl, param):
+    grepout = getoutput("grep '" + param + " ' " + logfl).split(None)
+    print(grepout)
+    
+    if grepout == []:
+        return leave_running_jobs_alone # Still running and leave_running_jobs_alone => 1
+    elif float(grepout[-1]) < 1.05:
+        return 1
+    else:
+        return 0
+    
+
+leave_running_jobs_alone = int(sys.argv[1])
+
+for logfl in glob.glob("UNITY*/log.txt"):
+    check_Om = check_param(logfl, "Om", leave_running_jobs_alone)
+    check_wDE = check_param(logfl, "wDE", leave_running_jobs_alone)
+
+    if check_Om and check_wDE:
+        print("Good!")
+    else:
+        cmd = "cd " + logfl.split("/")[0] + "\nsbatch run.sh"
+        print(cmd)
+        print(getoutput(cmd))
