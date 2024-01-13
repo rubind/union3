40136d8017acde724cd47e71f0df3ed17099d894
David Rubin <drubin@cn-03-13-02.mana.ci.its.hawaii.edu>
2024-01-13 16:36:15 +0000

    sim updates

--- simulated/check_Om.sh
+++ simulated/check_Om.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=shared
 #SBATCH --time=0-18:00:00 ## time format is DD-HH:MM:SS
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=6G # Memory per node my job requires
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G # Memory per node my job requires
 #SBATCH --error=example-%A.err # %A - filled with jobid, where to write the stderr
 #SBATCH --output=example-%A.out # %A - filled with jobid, wher to write the stdout
 source ~/.bash_profile
@@ -14,10 +14,5 @@ do
     cd /home/drubin/koa_scratch/sim_data_simplex/
     python /home/drubin/union3//simulated/step1B_Om_sampled_well.py 1
     
-    sleep 60
-    
-    cd /home/drubin/koa_scratch/sim_data_simplex_20/
-    python /home/drubin/union3//simulated/step1B_Om_sampled_well.py 1
-
     sleep 1800
 done
--- simulated/step1B_Om_sampled_well.py
+++ simulated/step1B_Om_sampled_well.py
@@ -5,13 +5,13 @@ except:
 import sys
 import glob
 
-def check_param(logfl, param, leave_running_jobs_alone):
+def check_param(logfl, param, leave_running_jobs_alone, threshold = 1.05):
     grepout = getoutput("grep '" + param + " ' " + logfl).split(None)
     print(grepout)
     
     if grepout == []:
         return leave_running_jobs_alone # Still running and leave_running_jobs_alone => 1
-    elif float(grepout[-1]) < 1.05:
+    elif float(grepout[-1]) < threshold:
         return 1
     else:
         return 0
@@ -28,7 +28,12 @@ for logfl in glob.glob("UNITY*/log.txt"):
     check_Om = check_param(logfl, param = "Om", leave_running_jobs_alone = leave_running_jobs_alone)
     check_wDE = check_param(logfl, param = "wDE", leave_running_jobs_alone = leave_running_jobs_alone)
 
-    if check_Om and check_wDE:
+    other_checks = 1
+    for key in ["beta_B"]:
+        other_checks *= check_param(logfl, param = key, leave_running_jobs_alone = leave_running_jobs_alone, threshold = 1.2)
+
+        
+    if check_Om and check_wDE and other_checks:
         print("Good!")
     else:
         cmd = "cd " + logfl.split("/")[0] + "\nsbatch run.sh"
