2244d0d0e6a0431bf129cfe0253cf9d6a4209887
David Rubin <drubin@hawaii.edu>
2024-01-15 07:15:08 -1000

    step2 reads pickles

--- simulated/step2_summarize.py
+++ simulated/step2_summarize.py
@@ -1,12 +1,11 @@
 import glob
-try:
-    from commands import getoutput
-except:
-    from subprocess import getoutput
-
+import pickle
+import gzip
+from scipy.stats import scoreatpercentile
 import numpy as np
 from FileRead import read_param
 import sys
+import tqdm
 
 def fmt(val, unc, mean_unc):
     if np.isnan(val):
@@ -28,21 +27,21 @@ if suffix == "H":
 else:
     cosmomodel = "5"
 
-pars = ["Om"]*(cosmomodel == "1") + ["wDE", "waDE"]*(cosmomodel == "5") + ["alpha", "beta_B", "beta_R_low", "beta_R_high", "delta_0", "delta_h", "mobs_cuts\[1\]", "mobs_cut_sigmas\[1\]", "sigma_int\[1\]", "mBx1c_int_variance\[1\]", "mBx1c_int_variance\[2\]", "mBx1c_int_variance\[3\]"]
+pars = ["Om"]*(cosmomodel == "1") + ["wDE", "waDE"]*(cosmomodel == "5") + ["alpha", "beta_B", "beta_R_low", "beta_R_high", "delta_0", "delta_h", "mobs_cuts[1]", "mobs_cut_sigmas[1]", "sigma_int[1]", "mBx1c_int_variance[1]", "mBx1c_int_variance[2]", "mBx1c_int_variance[3]"]
 
 
-labels = {"Om": "$\Omega_m$", "wDE": "$w_0$", "waDE": "$w_a$", "this_MB": "$\mathcal{M}_B$", "alpha": "$\\alpha$",
+labels = {"Om": "$\Omega_m$", "wpivot": "$w_0 + 0.15\;w_a$", "waDE": "$w_a$", "this_MB": "$\mathcal{M}_B$", "alpha": "$\\alpha$",
           "beta_B": "$\\beta_B$",
           "beta_R_low": "$\\beta_{RL}$",
           "beta_R_high": "$\\beta_{RH}$",
           "delta_0": "$\delta(0)$",
           "delta_h": "$\delta(\infty)/\delta(0)$",
-          "mobs_cuts\[1\]": "$m_{50}$",
-          "mobs_cut_sigmas\[1\]": "$\sigma_m$",
-          "sigma_int\[1\]": "$\sigma^{\mathrm{unexpl}}$",
-          "mBx1c_int_variance\[1\]": "$f^{m_B}$",
-          "mBx1c_int_variance\[2\]": "$f^{x_1}$",
-          "mBx1c_int_variance\[3\]": "$f^{c}$"}
+          "mobs_cuts[1]": "$m_{50}$",
+          "mobs_cut_sigmas[1]": "$\sigma_m$",
+          "sigma_int[1]": "$\sigma^{\mathrm{unexpl}}$",
+          "mBx1c_int_variance[1]": "$f^{m_B}$",
+          "mBx1c_int_variance[2]": "$f^{x_1}$",
+          "mBx1c_int_variance[3]": "$f^{c}$"}
 
 
 """
@@ -63,13 +62,13 @@ for x1c	in ["x1", "c"]:
 all_txt_grid = []
 
 for matchstr, description in [
-        ("UNITY" + suffix + "_nosel_cos=" + cosmomodel + "_???/log.txt", "No Selection Effects"),
-        ("UNITY" + suffix + "_nosel_twopop_cos=" + cosmomodel + "_???/log.txt", "No Selection Effects"),
-        ("UNITY" + suffix + "_cos=" + cosmomodel + "_???/log.txt", "Nominal UNITY1.5 Model")]:
+        ("UNITY" + suffix + "_nosel_cos=" + cosmomodel + "_???", "No Selection Effects"),
+        ("UNITY" + suffix + "_nosel_twopop_cos=" + cosmomodel + "_???", "No Selection Effects"),
+        ("UNITY" + suffix + "_cos=" + cosmomodel + "_???", "Nominal UNITY1.5 Model")]:
         #("UNITY" + suffix + "_1D_???/log.txt", "UNITY1.5, 1D Unexplained"),
         #("UNITY" + suffix + "_nocal_???/log.txt", "UNITY1.5, No $\Delta$sys")
 
-    logs = glob.glob(matchstr)
+    sampfls = glob.glob(matchstr + "/*samples*pickle")
     
 
     all_pars = {}
@@ -78,19 +77,23 @@ for matchstr, description in [
     for par in pars:
         all_pars[par] = []
         all_uncs[par] = []
+    
+    for sampfl in tqdm.tqdm(sampfls):
+        fit_params = pickle.load(gzip.open(sampfl, 'rb'))
+        fit_params["wpivot"] = fit_params["wDE"] + 0.15*fit_params["waDE"]
+        
+        for par in pars:
+            if par.count("[") == 1:
+                ind = int(par.split("[")[-1].split("]")[0])
+                all_pars[par].append(np.median(fit_params[par][:, ind - 1]))
+                all_uncs[par].append(0.5*(scoreatpercentile(fit_params[par][:,ind - 1], 84.1345) - scoreatpercentile(fit_params[par][:, ind - 1], 15.8655)))
 
-        for log in logs:
-            val = getoutput("grep '" + par + " ' " + log)
-            try:
-                themean = float(val.split(None)[1])
-                theunc = float(val.split(None)[3])
-                all_pars[par].append(themean)
-                all_uncs[par].append(theunc)
+            else:
+                all_pars[par].append(np.median(fit_params[par]))
+                all_uncs[par].append(0.5*(scoreatpercentile(fit_params[par], 84.1345) - scoreatpercentile(fit_params[par], 15.8655)))
+
                 
-            except:
-                print("SKIPPING", val, log, par)
-
-            if ["mBx1c_int_variance\[1\]", "mBx1c_int_variance\[2\]", "mBx1c_int_variance\[3\]"].count(par):
+            if ["mBx1c_int_variance[1]", "mBx1c_int_variance[2]", "mBx1c_int_variance[3]"].count(par):
                 if read_param(log.split("/")[0] + "/paramfile.txt", "threeD_unexplained") == 0:
                     if par == "mBx1c_int_variance\[1\]":
                         all_pars[par][-1] = 1
@@ -98,6 +101,7 @@ for matchstr, description in [
                     else:
                         all_pars[par][-1] = 0
                         all_uncs[par][-1] = 0                        
+
             if par.count("mobs_cut"):
                 if read_param(log.split("/")[0] + "/paramfile.txt", "stan_code").count("no_sel"):
                     all_pars[par][-1] = np.sqrt(-1)
