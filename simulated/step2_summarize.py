5994d61f566485e111c845f13d57528a20a4af95
David Rubin <drubin@cn-03-13-03.mana.ci.its.hawaii.edu>
2024-01-17 02:18:36 +0000

    sim updates

--- simulated/step2_summarize.py
+++ simulated/step2_summarize.py
@@ -6,6 +6,12 @@ import numpy as np
 from FileRead import read_param
 import sys
 import tqdm
+import matplotlib.pyplot as plt
+import multiprocessing
+multiprocessing.set_start_method("fork")
+import pystan 
+
+
 
 def fmt(val, unc, mean_unc):
     if np.isnan(val):
@@ -27,10 +33,18 @@ if suffix == "H":
 else:
     cosmomodel = "5"
 
-pars = ["Om"]*(cosmomodel == "1") + ["wpivot", "waDE"]*(cosmomodel == "5") + ["alpha", "beta_B", "beta_R_low", "beta_R_high", "delta_0", "delta_h", "mobs_cuts[1]", "mobs_cut_sigmas[1]", "sigma_int[1]", "mBx1c_int_variance[1]", "mBx1c_int_variance[2]", "mBx1c_int_variance[3]"]
+datasetkeys = []
+    
+for tmpind in range(1 + (suffix == "LH")*2):
+    datasetkeys.append("mobs_cuts[%i]" % (tmpind + 1))
+    datasetkeys.append("mobs_cut_sigmas[%i]" % (tmpind + 1))
+    datasetkeys.append("sigma_int[%i]" % (tmpind + 1))
+
+    
+pars = ["Om"]*(cosmomodel == "1") + ["wDE", "wpivot", "waDE"]*(cosmomodel == "5") + ["alpha", "beta_B", "beta_R_low", "beta_R_high", "delta_0", "delta_h"] + datasetkeys  + ["mBx1c_int_variance[1]", "mBx1c_int_variance[2]", "mBx1c_int_variance[3]"]
 
 
-labels = {"Om": "$\Omega_m$", "wpivot": "$w_0 + 0.15\;w_a$", "waDE": "$w_a$", "this_MB": "$\mathcal{M}_B$", "alpha": "$\\alpha$",
+labels = {"Om": "$\Omega_m$", "wDE": "$w_0$", "wpivot": "$w_0 + 0.15\;w_a$", "waDE": "$w_a$", "this_MB": "$\mathcal{M}_B$", "alpha": "$\\alpha$",
           "beta_B": "$\\beta_B$",
           "beta_R_low": "$\\beta_{RL}$",
           "beta_R_high": "$\\beta_{RH}$",
@@ -43,7 +57,12 @@ labels = {"Om": "$\Omega_m$", "wpivot": "$w_0 + 0.15\;w_a$", "waDE": "$w_a$", "t
           "mBx1c_int_variance[2]": "$f^{x_1}$",
           "mBx1c_int_variance[3]": "$f^{c}$"}
 
-
+if suffix == "LH":
+    for tmpind, tmpkey in enumerate(["Low", "Mid", "High"]):
+        labels["mobs_cuts[%i]" % (tmpind + 1)] = "$m_{50}$ %s-$z$" % tmpkey
+        labels["mobs_cut_sigmas[%i]" % (tmpind + 1)] = "$\sigma_m$ %s-$z$" % tmpkey
+        labels["sigma_int[%i]" % (tmpind + 1)] = "$\sigma^{\mathrm{unexpl}}$ %s-$z$" % tmpkey
+        
 """
 for x1c	in ["x1", "c"]:
     for	x1cfmt in ["%s_star", "R_%s", "tau_%s"]:
@@ -60,6 +79,9 @@ for x1c	in ["x1", "c"]:
 """
 
 all_txt_grid = []
+
+all_fmB_true = []
+all_fmB_posterior = []
 
 for matchstr, description in [
         ("UNITY" + suffix + "_nosel_cos=" + cosmomodel + "_???", "No Selection Effects"),
@@ -80,7 +102,26 @@ for matchstr, description in [
     
     for sampfl in tqdm.tqdm(sampfls):
         fit_params = pickle.load(gzip.open(sampfl, 'rb'))
-        fit_params["wpivot"] = fit_params["wDE"] + 0.15*fit_params["waDE"]
+        if cosmomodel == "5":
+            fit_params["wpivot"] = fit_params["wDE"] + 0.15*fit_params["waDE"]
+
+        if description == "Nominal UNITY1.5 Model":
+            fmB_true = read_param("params_" + sampfl.split("/")[0].split("_")[-1] + ".dat", "frac_var_mBx1c")
+            assert fmB_true[0] == "["
+            fmB_true = fmB_true.replace("[", "")
+            all_fmB_true.append(float(fmB_true))
+            all_fmB_posterior.append(fit_params["mBx1c_int_variance"][:,0])
+            
+        
+        filenamelist = read_param(sampfl.split("/")[0] + "/paramfile.txt", "filenamelist")
+        if suffix == "LH":
+            assert filenamelist[0].count("_L_")
+            assert filenamelist[1].count("_H_")
+            assert filenamelist[2].count("_V_")
+        elif suffix == "H":
+            assert filenamelist[0].count("_H_")
+        else:
+            raise "Uknown suffix " + suffix
         
         for par in pars:
             if par.count("[") == 1:
@@ -147,3 +188,101 @@ for i in range(len(pars)):
         towrite += "\\\\"
         print(towrite)
     
+
+
+skew_code = """
+data {
+    int n_samp;
+    vector [n_samp] posterior;
+}
+
+parameters {
+    real <lower = -4, upper = 4> mu;
+    real <lower = 0.01, upper = 5> sig;
+    real alpha;
+}
+
+model {
+    posterior ~ skew_normal(mu, sig, alpha);
+}
+"""
+
+try:
+    sk, sc = pickle.load(open("skew.pickle", 'rb'))
+    if sc != skew_code:
+        time_to_raise
+except:
+    sk = pystan.StanModel(model_code=skew_code)
+    pickle.dump([sm, skew_code], open("skew.pickle", 'wb'))
+
+        
+stan_code = """
+data {
+    int n_obs;
+
+    vector [n_obs] mu;
+    vector [n_obs] sig;
+    vector [n_obs] alpha;
+
+    vector [n_obs] xvals;
+    vector [100] pltx;
+}
+
+parameters {
+    real slope;
+    real intercept;
+}
+
+transformed parameters {
+    vector [100] plty;
+    plty = pltx*slope + intercept;
+}
+
+model {
+    for (i in 1:n_obs) {
+        slope*xvals[i] + intercept ~ skew_normal(mu[i], sig[i], alpha[i]);
+    }
+}
+"""
+
+try:
+    sm, sc = pickle.load(open("stan.pickle", 'rb'))
+    if sc != stan_code:
+        time_to_raise
+except:
+    sm = pystan.StanModel(model_code=stan_code)
+    pickle.dump([sm, stan_code], open("stan.pickle", 'wb'))
+
+stan_data = dict(n_obs = len(all_fmB_posterior), mu = [], sig = [], alpha = [], xvals = all_fmB_true, pltx = np.linspace(0, 1, 100))
+
+plt.figure(figsize = (5, 5))
+for i in tqdm.trange(len(all_fmB_posterior)):
+    plt.plot([all_fmB_true[i]]*2, scoreatpercentile(all_fmB_posterior[i], [15.8655, 84.1345]), color = 'b', zorder = 1)
+    plt.plot(all_fmB_true[i], np.median(all_fmB_posterior[i]), '.', color = 'b', zorder = 1)
+
+    fit = sk.sampling(data=dict(n_samp = len(all_fmB_posterior[i]), posterior = all_fmB_posterior[i]), iter=1000, chains=2, refresh = 1000)
+    print(fit)
+    fit_params = fit.extract(permuted = True)
+    stan_data["mu"].append(np.median(fit_params["mu"]))
+    stan_data["sig"].append(np.median(fit_params["sig"]))
+    stan_data["alpha"].append(np.median(fit_params["alpha"]))
+
+
+fit = sm.sampling(data=stan_data, iter=2000, chains=4, refresh = 100)
+print(fit)
+fit_params = fit.extract(permuted = True)
+
+plt.fill_between(stan_data["pltx"], scoreatpercentile(fit_params["plty"], 15.8655, axis = 0),
+                 scoreatpercentile(fit_params["plty"], 84.1345, axis = 0), color = 'b', zorder = -2, alpha = 0.5, label = "$y = %.2f x + %.2f$" % (np.median(fit_params["slope"]),
+                                                                                                                                                   np.median(fit_params["intercept"])))
+plt.legend(loc = 'best')
+
+
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.gca().set_aspect('equal')
+
+plt.xlabel("Simulation $f^{m_B}$")
+plt.ylabel("Posterior $f^{m_B}$")
+plt.savefig("f_mB_recovered_" + suffix + ".pdf", bbox_inches = 'tight')
+plt.close()
