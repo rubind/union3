66cf1d7fdf782a83864630a8c2a5573cf119a305
David Rubin <drubin@cn-03-13-01.mana.ci.its.hawaii.edu>
2025-04-28 05:55:30 +0000

    updates

--- simulated/step1A.sh
+++ simulated/step1A.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=step1n2
 #SBATCH --partition=shared
-#SBATCH --time=0-12:00:00 ## time format is DD-HH:MM:SS
+#SBATCH --time=0-22:00:00 ## time format is DD-HH:MM:SS
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=8G # Memory per node my job requires
@@ -9,7 +9,7 @@
 #SBATCH --output=step1n2-%A.out # %A - filled with jobid, wher to write the stdout
 source ~/.bash_profile
 
-cd /home/drubin/koa_scratch/sim_data_H0/
+cd /home/drubin/koa_scratch/sim_data_simplex_H0/
 python3.9 /home/drubin/union3//simulated/step1A_compareLC_vs_input.py read | tail
 python3.9 /home/drubin/union3//simulated/step3_plot_sim_data.py read | tail
 
