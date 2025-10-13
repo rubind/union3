be5fa2331798dd97ca9a7c4d85dbeefdc36e829b
Samuel Hinton <samuelreay@gmail.com>
2025-10-13 16:33:37 +1000

    Reworking the entrypoint

---
+++ src/union3/config.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+from typing import Self
+from pydantic import Field, field_validator, model_validator
+
+from union3.utils.base_config import FileConfig
+
+
+class Config(FileConfig):
+    base: str | None = Field(
+        default=None,
+        description="Base config YAML file in the configs directory. If None, will use the defaults from config.py",
+    )
+
+    #! Config to control what gets run
+    cache_data_processing: bool = Field(
+        default=True, description="Use caching for data processing and stan running if possible."
+    )
+    cache_model_fitting: bool = Field(default=True, description="Use caching for stan model fitting if possible.")
+    do_plotting: bool = Field(default=True, description="Generate plots after running.")
+
+    #! Cosmology model config
+    model: str = Field(default="unity_stan_1.8.txt", description="Stan model file in the models directory.")
+    iterations: int = Field(default=2500, ge=1, description="Number of iterations for MCMC.")
+    num_jobs: int = Field(default=4, ge=1, description="Number of parallel jobs for MCMC.")
+    num_chains: int = Field(default=4, ge=1, description="Number of chains for MCMC.")
+    max_params_to_save: int = Field(default=1000, ge=1, description="Maximum number of parameters to save from MCMC.")
+    do_host_mass: bool = Field(default=True, description="Whether to include host mass step correction.")
+    fix_omega_m: bool = Field(default=False, description="Whether to fix Omega_m during fitting to 0.3.")
+    MB_by_sample: bool = Field(default=False, description="Whether to fit for different absolute magnitude by sample.")
+    include_peculiar_velocity_covariance: bool = Field(
+        default=False, description="Whether to include peculiar velocity covariance matrix."
+    )
+    separate_mass_x1c: bool = Field(
+        default=True, description="Whether to separate x1 and color standardization by host mass."
+    )
+
+    #! Data processing config
+    do_blinding: bool = Field(default=True, description="Whether to blind the data.")
+    min_redshift: float = Field(default=0.01, ge=0.0, description="Cut on minimum redshift.")
+    max_redshift: float = Field(default=3.0, ge=0.0, description="Cut on maximum redshift.")
+    max_first_phase: float = Field(default=100.0, description="Cut on maximum first phase.")
+    min_last_phase: float = Field(default=-100.0, description="Cut on minimum last phase.")
+    max_color_uncertainty: float = Field(default=0.2, ge=0.0, description="Cut on maximum color uncertainty.")
+    min_color: float = Field(default=-0.3, description="Cut on minimum color.")
+    max_color: float = Field(default=0.3, description="Cut on maximum color.")
+    max_MWEBV: float = Field(default=0.3, description="Cut on maximum Milky Way E(B-V).")
+
+    #! Data augmentation config
+    peculiar_velocity_dispersion: float = Field(
+        default=0.001, ge=0.0, description="Peculiar velocity dispersion in units of c."
+    )
+    lensing_dispersion: float = Field(
+        default=0.055, ge=0.0, description="Lensing dispersion in magnitudes per redshift."
+    )
+    MWEBV_zeropoint_EBV: float = Field(default=0.005, ge=0.0, description="Zeropoint uncertainty on Milky Way E(B-V).")
+    outlier_fraction: float = Field(default=0.02, ge=0.0, le=1.0, description="Fraction of outliers in the sample.")
+    # blinding_file?
+    # filename_list
+    # weird_sn_list
+    # mag_cuts
+    # sample file
+    # calibration_uncertainties
+    # max params to save
+
+    @property
+    def model_path(self) -> Path:
+        return self.model_dir() / self.model
+
+    @classmethod
+    def model_dir(cls):
+        return Path(__file__).parent / "models"
+
+    @field_validator("model")
+    def validate_stan_model(cls, v: str) -> str:
+        names = [p.name for p in cls.model_dir().glob("*.txt")]
+        assert v in names, f"Model {v} not found in models directory, options are {names}"
+        return v
+
+    @model_validator(mode="after")
+    def validate_model(self) -> Self:
+        assert self.min_redshift < self.max_redshift, "min_redshift must be less than max_redshift"
+        return self
--- src/union3/main.py
+++ src/union3/main.py
@@ -1,95 +1,12 @@
-from pathlib import Path
-import pickle
-import multiprocessing
-
-import stan
-import gzip
-
-from union3.utils.init_fn import init_fn
-from union3.utils.augment import add_zbins
-from union3.utils.blind import blind
+from union3 import logger
+from union3.config import Config
 
 
 def main():
-    multiprocessing.set_start_method("fork")
+    config = Config()
+    logger.info(f"Running Unity with base config file: {config.base}")
+    logger.info(f"Run settings: {config.model_dump_json(indent=2)}")
 
-    here = Path(__file__).parent
-    inputfl = (
-        here
-        / "data"
-        / "inputs_Amanullah10_CNIa02_CSP_CalanTololo_CfA1_CfA2_CfA3_CfA4_DES3_Deep_DES3_Shallow_ESSENCE_Foundation_LOSS_MCT_NB99_Pan-STARRS_Riess07_SDSS_SNLS_SuzukiRubin_Tonry03_LSQ+LCO_LSQ_knop03_Krisciunas.pickle"
-    )
-    print(
-        "cosmo_model: 1 for Om, 2 for binned mu, 3 for Omega_m-w, 4 for q0-j0, 5 for Omega_m-w0-wa, 6 for binned mu with comoving interpolation"
-    )
-    # cosmo_model = int(sys.argv[2])
-    cosmo_model = 1
-
-    (the_data, stan_data, params) = pickle.load(gzip.open(inputfl, "rb"))
-
-    stan_data = add_zbins(stan_data, cosmo_model)
-
-    print("nzadd ", stan_data["nzadd"])
-
-    if stan_data["do_blind"]:
-        stan_data, the_data = blind(stan_data, the_data, params)
-    else:
-        print("Not Blinding!")
-
-    print("Running...")
-
-    stan_file = here / "models" / "stan_code_H0_1.8.txt"
-    stan_model = stan_file.read_text()
-
-    posterior = stan.build(stan_model, data=stan_data)
-    fit = posterior.sample(
-        num_samples=10,  # params["iter"],
-        num_warmup=10,
-        num_chains=params["chains"],
-        # n_jobs=params["n_jobs"],
-        # refresh=10,
-        init=[init_fn(stan_data, the_data) for _ in range(params["chains"])],
-        # sample_file=params["sample_file"],
-    )
-
-    samples = fit.to_frame()
-    samples.to_parquet("samples.parquet")
-
-
-# try:
-#     fit_params = helper_functions.filter_fit_params(
-#         fit_params, "MB", params["chains"], params["iter"] / 2
-#     )  # burns the first half of the chain, so iter/2
-# except:
-#     print("Couldn't filter bad chains! One or more chains may be bad!")
-
-# # summarize_parameters(fit_params)
-
-# helper_functions.write_latent_variables(the_data=the_data, stan_data=stan_data, fit_params=fit_params)
-
-
-# del_keys = []
-# for key in fit_params:
-#     sh = np.array(fit_params[key].shape)
-
-#     if np.any(sh[1:] > params["max_params_to_save"]):
-#         print(key, " is too big to save!", sh)
-#         del_keys.append(key)
-
-# print("del_keys", del_keys)
-# for key in del_keys:
-#     del fit_params[key]
-
-
-# pickle.dump(fit_params, gzip.open("samples.pickle", "wb"))
-
-
-# print("I hope you have a log file:")
-
-# try:
-#     print(fit.stansummary(digits_summary=5))
-# except:
-#     print("Couldn't print fit! Something is very wrong!")
 
 if __name__ == "__main__":
     main()
