3696bd660a6e8a56671a8081bb99fee61fe681ba
Samuel Hinton <samuelreay@gmail.com>
2025-11-11 08:31:30 +1000

    Adding entrypoint

--- src/union3/__init__.py
+++ src/union3/__init__.py
@@ -1,9 +1,8 @@
 from loguru import logger
-from rich.logging import RichHandler
 from union3.config import Config, CosmologyModel
 from union3.data import Data
 from union3.models.models import Model
+from union3.main import fit_cosmology
 
-logger.configure(handlers=[{"sink": RichHandler(markup=True), "format": "{message}"}])
 
-__all__ = ["logger", "Config", "CosmologyModel", "Data", "Model"]
+__all__ = ["logger", "Config", "CosmologyModel", "Data", "Model", "fit_cosmology"]
--- src/union3/main.py
+++ src/union3/main.py
@@ -1,10 +1,12 @@
 from union3 import logger
 from union3 import Config, Data, Model
+import polars as pl
 # from union3.plotting import plot_approx_hubble_diagram
 
 
-def main():
-    config = Config()
+def fit_cosmology(config: Config | None = None) -> pl.DataFrame:
+    if config is None:
+        config = Config()
     logger.info(f"Running Unity with base config file: {config.base}")
     logger.info(f"Run settings: {config.model_dump_json(indent=2)}")
 
@@ -14,11 +16,17 @@ def main():
     model.initialise(data)
 
     samples = model.fit()
+
+    # TODO: make this path configurable and part of the config
     samples.write_parquet(config.output_dir / "mcmc_samples.parquet")
 
     print(samples.describe())
     # plot_approx_hubble_diagram(data.filtered_supernova, config)
+    return samples
 
 
 if __name__ == "__main__":
-    main()
+    from rich.logging import RichHandler
+
+    logger.configure(handlers=[{"sink": RichHandler(markup=True), "format": "{message}"}])
+    fit_cosmology()
