dc12e284b40dcbeffd6faa30e8f28f7f30fa08ad
David Rubin <drubin@cn-03-13-01.mana.ci.its.hawaii.edu>
2026-01-06 05:08:29 +0000

    w0wa

--- cobaya/w0wa.py
+++ cobaya/w0wa.py
@@ -8,32 +8,43 @@ def twoD_chi2_to_sigma(x):
     return np.sqrt(2) * erfinv(1 - np.exp(-x / 2.0))
 
 
-[weight, minuslogpost, cosmomc_theta, ombh2, omch2, tau, ns, logA, w, wa] = readcol(sys.argv[1], 'i,f,f,ff,f,f,f,ff')
+burn = 0.3
 
-print("weight", set(weight))
+for item in sys.argv[1:]:
+    [weight, minuslogpost, cosmomc_theta, ombh2, omch2, tau, ns, logA, w, wa] = readcol(item, 'i,f,f,ff,f,f,f,ff')
+    
+    print("weight", set(weight))
+    
+    full_w = []
+    full_wa = []
+    
+    for samp in tqdm.trange(len(weight)):
+        for j in range(weight[samp]):
+            full_w.append(w[samp])
+            full_wa.append(wa[samp])
+        
+    print(len(full_w))
+    print(len(full_wa))
 
-full_w = []
-full_wa = []
+    min_sample = int(len(full_w)*burn)
 
-for samp in tqdm.trange(len(weight)):
-    for j in range(weight[samp]):
-        full_w.append(w[samp])
-        full_wa.append(wa[samp])
+    full_w = full_w[min_sample:]
+    full_wa = full_wa[min_sample:]
 
-print(len(full_w))
-print(len(full_wa))
-
-samps = np.array([full_w, full_wa])
-cmat = np.cov(samps)
-print(cmat, np.sqrt(np.diag(cmat)))
-
-wmat = np.linalg.inv(cmat)
-med = np.median(samps, axis = 1)
-print(med)
-
-resid = np.array([-1., 0.]) - med
-print("resid", resid)
-
-chi2_LCDM = np.dot(resid, np.dot(wmat, resid))
-print("chi2 of -1 0 ", sys.argv[1], chi2_LCDM, " sigma: ", twoD_chi2_to_sigma(chi2_LCDM))
+    print(len(full_w))
+    print(len(full_wa))
+    
+    samps = np.array([full_w, full_wa])
+    cmat = np.cov(samps)
+    print(cmat, np.sqrt(np.diag(cmat)))
+    
+    wmat = np.linalg.inv(cmat)
+    med = np.median(samps, axis = 1)
+    print(med)
+    
+    resid = np.array([-1., 0.]) - med
+    print("resid", resid)
+    
+    chi2_LCDM = np.dot(resid, np.dot(wmat, resid))
+    print("chi2 of -1 0 ", item, chi2_LCDM, " sigma: ", twoD_chi2_to_sigma(chi2_LCDM))
 
