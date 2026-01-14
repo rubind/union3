9b622c1584ac31efb4706f03f7955b2c968ff370
David Rubin <drubin@cn-03-23-03.mana.ci.its.hawaii.edu>
2026-01-14 22:31:19 +0000

    w0wa

--- cobaya/w0wa.py
+++ cobaya/w0wa.py
@@ -11,7 +11,23 @@ def twoD_chi2_to_sigma(x):
 burn = 0.3
 
 for item in sys.argv[1:]:
-    [weight, minuslogpost, cosmomc_theta, ombh2, omch2, tau, ns, logA, w, wa] = readcol(item, 'i,f,f,ff,f,f,f,ff')
+    f = open(item, 'r')
+    lines = f.read().split('\n')
+    f.close()
+
+    weight_ind = lines[0].split(None).index("weight") - 1
+    w_ind = lines[0].split(None).index("w") -	1
+    wa_ind = lines[0].split(None).index("wa") -   1
+
+    assert weight_ind == 0
+    assert w_ind < 10 and w_ind > 0
+    assert wa_ind < 10 and wa_ind > 0
+
+    first_cols = readcol(item, 'i,ffffffffff')
+
+    weight = first_cols[weight_ind]
+    w = first_cols[w_ind]
+    wa = first_cols[wa_ind]
     
     print("weight", set(weight))
     
@@ -40,11 +56,14 @@ for item in sys.argv[1:]:
     
     wmat = np.linalg.inv(cmat)
     med = np.median(samps, axis = 1)
+    av = np.mean(samps, axis = 1)
     print(med)
-    
-    resid = np.array([-1., 0.]) - med
-    print("resid", resid)
-    
-    chi2_LCDM = np.dot(resid, np.dot(wmat, resid))
-    print("chi2 of -1 0 ", item, chi2_LCDM, " sigma: ", twoD_chi2_to_sigma(chi2_LCDM))
+    print(av)
+
+    for cent in [med, av]:
+        resid = np.array([-1., 0.]) - cent
+        print("resid", resid)
+        
+        chi2_LCDM = np.dot(resid, np.dot(wmat, resid))
+        print("chi2 of -1 0 ", item, chi2_LCDM, " sigma: ", twoD_chi2_to_sigma(chi2_LCDM))
 
